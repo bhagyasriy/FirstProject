8099197ec79025be450636b1e3f0d0ec0a9a47a3
Bhagya <bhagyacts@gmail.com>
2023-01-07 21:10:30 +0530

    changed HashSet file

--- HashSetDemoFile.java
+++ HashSetDemoFile.java
@@ -1,28 +1,32 @@
 package com.code;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 
 public class HashSetDemo {
 
 	public static void main(String[] args) {
-		
-		//insertion order is not maintained
-		HashSet hs =  new HashSet();
+
+		// insertion order is not maintained
+		HashSet hs = new HashSet();
 		hs.add(50);
 		hs.add(100);
 		hs.add(25);
 		hs.add(10);
 		System.out.println(hs);
-		
-		//insertion order is maintained
-		LinkedHashSet lhs =  new LinkedHashSet();
-		
+
+		// insertion order is maintained
+		LinkedHashSet lhs = new LinkedHashSet();
+
 		lhs.add(50);
 		lhs.add(100);
 		lhs.add(25);
 		lhs.add(10);
-		System.out.println(lhs);
+		Iterator<E> itr = lhs.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 
 	}
 
